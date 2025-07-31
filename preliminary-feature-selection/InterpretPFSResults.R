43b8fb91a218f3358d2d6b605d7383d9464c6e82
WesLFletch <weslfletch@tamu.edu>
2025-07-31 14:28:38 -0500

    WF revert to empty

--- preliminary-feature-selection/InterpretPFSResults.R
+++
@@ -1,9 +0,0 @@
-####################### INTERPRET THE RESULTS OF THE PRELIMINARY FS ######################
-
-setwd("YOUR-DIRECTORY-HERE") # FIXME: set to appropriate directory
-
-prelim_fs = readRDS("PrelimFSResults.RDS")
-prelim_fs_clean = prelim_fs[rowSums(is.na(prelim_fs)) == 0,]
-mrna_to_keep = prelim_fs_clean$gene[prelim_fs_clean$adj_r_sq < 0.85]
-
-saveRDS(mrna_to_keep, file = "UncorMRNA.RDS")
